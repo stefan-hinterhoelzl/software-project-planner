8c0ccd870bb08c171495915588373e66b2672563
Stefan Hinterhölzl <hinterhoelzlstefan@gmail.com>
2022-12-10 18:59:43 +0100

    firebase init

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyCe-t12yNgLqCgU0MIObWOrNBK6QbHDOts",
+        authDomain: "software-project-planner.firebaseapp.com",
+        projectId: "software-project-planner",
+        storageBucket: "software-project-planner.appspot.com",
+        messagingSenderId: "739803063205",
+        appId: "1:739803063205:web:5517df376fc9d293cdfe4c"
+    },
+}
