a13b57c1136143353db297316c2d0907f52d8f59
stefan-hinterhoelzl <hinterhoelzlstefan@gmail.com>
2023-04-07 08:08:16 +0200

    project config view

--- Frontend/src/app/projects/project-view/project-config-view/project-config-view.component.ts
+++ Frontend/src/app/projects/project-view/project-config-view/project-config-view.component.ts
@@ -34,7 +34,6 @@ export class ProjectConfigViewComponent {
     })
   );
 
-  //remoteProjects$ = this.project$.pipe(switchMap(project => this.backend.getRemoteProjectsForProject(project!.projectId)));
   remoteProjects$ = this.data.remoteProjects$;
 
   hide: boolean = true;
