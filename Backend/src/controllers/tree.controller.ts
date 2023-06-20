66cf80ecbf6b99452735f4f4a4f5f2f6a96ece4d
stefan-hinterhoelzl <hinterhoelzlstefan@gmail.com>
2023-06-20 15:46:08 +0200

    backend functions

--- Backend/src/controllers/tree.controller.ts
+++ Backend/src/controllers/tree.controller.ts
@@ -1,8 +1,32 @@
 import { Request, Response } from "express";
+import { IssueNode } from "../models/nodes";
 
+//*** Tree Evaluation ***/
 export async function evaluateTree(req: Request, res: Response) {
-//TODO
+    let tree: IssueNode[] = req.body;
+
+    tree.forEach((value, index, arr) => {
+        checkDeadlines(value)
+    })
 }
+
+
+
+const checkDeadlines = (node: IssueNode): void => {
+  
+
+
+}
+
+
+const markNodeAndParents = (node: IssueNode): void => {
+
+
+
+}
+
+
+
 
 
 export async function detectHierarchies(req: Request, res: Response) {
